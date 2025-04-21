453cb0c2329043e6aaf15308f80219637c404280
hferr <hferrsj@gmail.com>
2025-04-21 12:14:29 -0300

    adds new repo funcs to interface

--- internal/app/repo.go
+++ internal/app/repo.go
@@ -9,4 +9,7 @@ import (
 type Repo interface {
 	FindAgentByID(ctx context.Context, ID uuid.UUID) (Agent, error)
 	UpsertAgent(ctx context.Context, agent Agent) (Agent, error)
+
+	FindCustomerByID(ctx context.Context, ID uuid.UUID) (Customer, error)
+	IsCustomerConnectedToAgent(ctx context.Context, customerID, agentID uuid.UUID) (bool, error)
 }
