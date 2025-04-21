a27fbb938c270fa5dc4423165882ce537abad159
hferr <hferrsj@gmail.com>
2025-04-21 11:18:46 -0300

    adds error handling in responses

--- internal/httpjson/response.go
+++ internal/httpjson/response.go
@@ -1,6 +1,10 @@
 package httpjson
 
 import (
+	"errors"
+	"net/http"
+
+	"github.com/go-playground/validator/v10"
 	"github.com/google/uuid"
 	"github.com/hferr/hw-rest-api/internal/app"
 	"github.com/labstack/echo/v4"
@@ -26,6 +30,45 @@ func newAgentResponse(agent app.Agent) agentResponse {
 		CreatedAt:   agent.CreatedAt.String(),
 		UpdatedAt:   agent.UpdatedAt.String(),
 	}
+}
+
+var errStatusMap = map[error]int{
+	app.ErrInternal:      http.StatusInternalServerError,
+	app.ErrAgentNotFound: http.StatusNotFound,
+}
+
+type errResponse struct {
+	Errors []string `json:"errors"`
+}
+
+func newErrResponse(errors []string) errResponse {
+	return errResponse{Errors: errors}
+}
+
+func parseError(err error) []string {
+	var errMsgs []string
+	if errors.As(err, &validator.ValidationErrors{}) {
+		for _, err := range err.(validator.ValidationErrors) {
+			errMsgs = append(errMsgs, err.Error())
+		}
+	} else {
+		errMsgs = append(errMsgs, err.Error())
+	}
+
+	return errMsgs
+}
+
+func handleError(c echo.Context, err error) error {
+	statusCode, ok := errStatusMap[err]
+	if !ok {
+		statusCode = http.StatusInternalServerError
+	}
+
+	return c.JSON(statusCode, newErrResponse(parseError(err)))
+}
+
+func handleValidationError(c echo.Context, err error) error {
+	return c.JSON(http.StatusBadRequest, newErrResponse(parseError(err)))
 }
 
 func handleSuccess(c echo.Context, statusCode int, data any) error {
