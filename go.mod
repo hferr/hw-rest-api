e108522160594930a04ad811408bbef2956e9f05
hferr <hferrsj@gmail.com>
2025-04-21 09:46:38 -0300

    adds agent model

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/hferr/hw-rest-api
 go 1.24.1
 
 require (
+	github.com/google/uuid v1.6.0
 	github.com/jackc/pgx/v5 v5.7.4
 	github.com/joeshaw/envdecode v0.0.0-20200121155833-099f1fc765bd
 	github.com/labstack/echo/v4 v4.13.3
---
+++ internal/app/agent_model.go
@@ -0,0 +1,29 @@
+package app
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Agent struct {
+	ID          uuid.UUID
+	Name        string
+	Email       string
+	PhoneNumber string
+	Location    string
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
+
+func NewAgent(name, email, phoneNumber, location string) Agent {
+	return Agent{
+		ID:          uuid.New(),
+		Name:        name,
+		Email:       email,
+		PhoneNumber: phoneNumber,
+		Location:    location,
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	}
+}
